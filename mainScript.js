5b2b64a62392315f87f1963e2523887a4ae8358f
mahmoudzakeer7 <99131564+mahmoudzakeer7@users.noreply.github.com>
2024-02-18 03:49:54 +0200

    Add files via upload
    this after edit it

--- mainScript.js
+++ mainScript.js
@@ -6,9 +6,19 @@ const profileName = document.getElementsByName('profileName');
 const chartDash = document.querySelector('.chartDash');
 const irrigation = document.querySelector('.irrigation');
 const energy = document.querySelector('.energy');
+const control = document.querySelector('.control');
+const cropHelth = document.querySelector('.crop-helth');
+
+// boxes in dashbourd 
 const greenArea = document.querySelector('.green-area');
 const temp = document.querySelector('.temp');
-const control = document.querySelector('.control');
+const humidity = document.querySelector('.humidity');
+const soilMoisture = document.querySelector('.soil-moisture');
+const light = document.querySelector('.light');
+const ph = document.querySelector('.ph');
+const conductivity = document.querySelector('.conductivity');
+const rainfall = document.querySelector('.rainfall');
+const flowControl = document.querySelector('.flow-control');
 
 //menue
 const setting = document.querySelector('.setting');
@@ -16,96 +26,31 @@ const energyBox = document.querySelector('.energyBox');
 const crop = document.querySelector('.crop');
 const chartBox = document.querySelector('.chartBox');
 const water = document.querySelector('.water');
-const user = document.querySelector('.user');
+const products = document.querySelector('.products');
 const dash = document.querySelector('.dash');
 const exit = document.querySelector('.log-out');
 
-const params = new URLSearchParams(window.location.search);
-
-var options = {
-    series: [
-        {
-            name: "Actual",
-            data: [
-                {
-                    x: "2011",
-                    y: 12,
-                    meta: {
-                        "24-30 aug": 23,
-                        "16-23 aug": 20,
-                        "1-7 aug": 15
-                    }
-                },
-                {
-                    x: "2012",
-                    y: 44,
-                    meta: {
-                        "24-30 aug": 13,
-                        "16-23 aug": 10,
-                        "1-7 aug": 51
-                    }
-                },
-                {
-                    x: "2013",
-                    y: 54,
-                    meta: {
-                        "24-30 aug": 3,
-                        "16-23 aug": 0,
-                        "1-7 aug": 13
-                    }
-                },
-                {
-                    x: "2014",
-                    y: 66,
-                    meta: {
-                        "24-30 aug": 2,
-                        "16-23 aug": 2,
-                        "1-7 aug": 1
-                    }
-                }
-            ]
-        }
-    ],
-    chart: {
-        height: 350,
-        type: "bar"
-    },
-    plotOptions: {
-        bar: {
-            horizontal: true,
-        }
-    },
-    colors: ["#0ed75e"],
-
-    tooltip: {
-        custom: (opts) => {
-            const { seriesIndex, dataPointIndex, w } = opts;
-
-            const meta = w.config.series[seriesIndex].data[dataPointIndex].meta;
-            return Object.entries(meta)
-                .map(([k, v]) => {
-                    return `
-                <div>
-                  ${k} - ${v}  
-                </div>
-              `;
-                })
-                .join("");
-        }
-    }
-};
-
-var chart = new ApexCharts(document.querySelector("#chart-energy"), options);
-chart.render();
 
 window.onload = function () {
+
+    const params = new URLSearchParams(window.location.search);
+
+    temp.classList.remove('hide');
+    greenArea.classList.remove('hide');
+    humidity.classList.remove('hide');
+    soilMoisture.classList.remove('hide');
+    light.classList.remove('hide');
+    ph.classList.remove('hide');
+    conductivity.classList.remove('hide');
+    rainfall.classList.remove('hide');
+    flowControl.classList.remove('hide');
     chartDash.classList.add('hide');
     energy.classList.add('hide');
     irrigation.classList.add('hide');
-    temp.classList.remove('hide');
-    greenArea.classList.remove('hide');
     control.classList.add('hide');
-    profileName.textContent = params.get('userName');
+    cropHelth.classList.add('hide');
+
+    profileName.values = params.get('userName');
 }
 dash.addEventListener('click', () => {
     title.textContent = 'dashboard';
@@ -115,7 +60,32 @@ dash.addEventListener('click', () => {
     temp.classList.remove('hide');
     greenArea.classList.remove('hide');
     control.classList.add('hide');
+    humidity.classList.remove('hide');
+    soilMoisture.classList.remove('hide');
+    light.classList.remove('hide');
+    ph.classList.remove('hide');
+    conductivity.classList.remove('hide');
+    rainfall.classList.remove('hide');
+    flowControl.classList.remove('hide');
+});
 
+products.addEventListener('click', () => {
+    title.textContent = 'products';
+
+    cropHelth.classList.remove('hide');
+    energy.classList.add('hide');
+    chartDash.classList.add('hide');
+    irrigation.classList.add('hide');
+    temp.classList.add('hide');
+    greenArea.classList.add('hide');
+    control.classList.add('hide');
+    humidity.classList.add('hide');
+    soilMoisture.classList.add('hide');
+    light.classList.add('hide');
+    ph.classList.add('hide');
+    conductivity.classList.add('hide');
+    rainfall.classList.add('hide');
+    flowControl.classList.add('hide');
 
 });
 
@@ -127,7 +97,14 @@ water.addEventListener('click', () => {
     greenArea.classList.add('hide');
     temp.classList.add('hide');
     control.classList.add('hide');
-
+    humidity.classList.add('hide');
+    soilMoisture.classList.add('hide');
+    light.classList.add('hide');
+    ph.classList.add('hide');
+    conductivity.classList.add('hide');
+    rainfall.classList.add('hide');
+    flowControl.classList.add('hide');
+    cropHelth.classList.add('hide');
 
 });
 
@@ -139,6 +116,14 @@ energyBox.addEventListener('click', () => {
     temp.classList.add('hide');
     greenArea.classList.add('hide');
     control.classList.add('hide');
+    humidity.classList.add('hide');
+    soilMoisture.classList.add('hide');
+    light.classList.add('hide');
+    ph.classList.add('hide');
+    conductivity.classList.add('hide');
+    rainfall.classList.add('hide');
+    flowControl.classList.add('hide');
+    cropHelth.classList.add('hide');
 
 });
 
@@ -151,6 +136,15 @@ chartBox.addEventListener('click', () => {
     temp.classList.add('hide');
     greenArea.classList.add('hide');
     control.classList.add('hide');
+    humidity.classList.add('hide');
+    soilMoisture.classList.add('hide');
+    light.classList.add('hide');
+    ph.classList.add('hide');
+    conductivity.classList.add('hide');
+    rainfall.classList.add('hide');
+    flowControl.classList.add('hide');
+    cropHelth.classList.add('hide');
+
 
 });
 
@@ -162,8 +156,18 @@ setting.addEventListener('click', () => {
     temp.classList.add('hide');
     greenArea.classList.add('hide');
     control.classList.remove('hide');
+    humidity.classList.add('hide');
+    soilMoisture.classList.add('hide');
+    light.classList.add('hide');
+    ph.classList.add('hide');
+    conductivity.classList.add('hide');
+    rainfall.classList.add('hide');
+    flowControl.classList.add('hide');
+    cropHelth.classList.add('hide');
+
 });
 
 exit.addEventListener('click',()=>{
     location.assign('index.html');
 });
+
